327dbad01e85476f97fb9293e1da6414fde1d4f3
galiapsvwordplay <galiapsvwordplay@gmail.com>
2020-11-14 18:51:29 -0600

    footer

--- cmp/mi-footer.js
+++ cmp/mi-footer.js
@@ -1,7 +1,7 @@
 class MiFooter extends HTMLElement {
   connectedCallback() {
     this.innerHTML = /* html */
-      `Copyright &copy; 2020 Gilberto Pacheco Gallegos.`;
+      `Copyright &copy; 2020 Galia Paola Silva Villa.`;
   }
 }
 customElements.define("mi-footer", MiFooter);
