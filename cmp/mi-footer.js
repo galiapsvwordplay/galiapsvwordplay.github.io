b42e58cf71c37f6d59a249e275499026b2c8088a
galiapsvwordplay <galiapsvwordplay@gmail.com>
2020-11-14 19:11:09 -0600

    Modifique index y footer

--- cmp/mi-footer.js
+++ cmp/mi-footer.js
@@ -1,7 +1,7 @@
 class MiFooter extends HTMLElement {
   connectedCallback() {
     this.innerHTML = /* html */
-      `Copyright &copy; 2020 Galia Paola Silva Villa.`;
+      `Copyright &copy; 2020 Galia Silva Villa.`;
   }
 }
 customElements.define("mi-footer", MiFooter);
