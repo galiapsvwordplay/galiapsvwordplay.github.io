d4a396c31a29963accf3d4d59c7b469fba46d85e
galiapsvwordplay <galiapsvwordplay@gmail.com>
2020-11-15 20:48:30 -0600

    Agrege if

--- js/calcular.js
+++ js/calcular.js
@@ -1,4 +1,4 @@
-String.prototype.replace=function(index, character) { return this.substr(0, index) + 
+String.prototype.replaceAt=function(index, character) { return this.substr(0, index) + 
     character + this.substr(index+character.length); } 
 //Uso: palConGuiones = palConGuiones.replaceAt(i*2, letra);
 
@@ -7,17 +7,33 @@ const palabras = ["casa", "perro", "gato", "burro"];
 const palabra = palabras[Math.floor(Math.random()*palabras.length)];
 
 let palConGiones=palabra.replace(/./g, "_ ");
-
+let contFallos = 0;
 document.querySelector('#output').innerHTML =palConGiones;
-document.querySelector("#calcular").addEventListener("click" , () =>
+document.querySelector('#calcular').addEventListener('click' , () =>
 {
     const letra = document.querySelector('#letra').value;
-    
-    for (const i in palabra){
+    let fallo = true;
+    for (const i in palabra){   
         if(letra == palabra[i]){
-            palConGiones = palConGiones.replace(i * 2 , letra);
+            palConGiones = palConGiones.replaceAt(i * 2 , letra);
+            fallo=false;
         }
     }
+
+    if(fallo){
+        contFallos ++;
+        document.querySelector('#ahorcado').style.backgroundPosition = -(307 * contFallos) + 'px 0';
+        if(contFallos == 4){
+            alert("Game Over")
+        }
+    }else{
+        if(palConGiones.indexOf('_')<0){
+            alert("Ganaste")
+        }
+    }
+
     document.querySelector('#output').innerHTML =palConGiones;
+    document.querySelector('#letra').value = '';
+    document.querySelector('#letra').focus();
 
 });
