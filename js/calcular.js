fbde384e9e8d33df05a5023a4eca2c5dceface16
galiapsvwordplay <galiapsvwordplay@gmail.com>
2020-11-15 18:36:35 -0600

    Palabra con guiones

--- js/calcular.js
+++ js/calcular.js
@@ -1,3 +1,10 @@
+const palabras = ["casa", "perro", "gato", "burro"];
+
+const palabra = palabras[Math.floor(Math.random()*palabras.length)];
+
+const palConGiones=palabra.replace(/./g, "_ ");
+alert(palabra + " - " + palConGiones)
+
 document.querySelector('#calcular').addEventListener('click',() =>{
-    alert(1x)
-})
+alert(1)
+});
