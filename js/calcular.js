fe9fd81713a3228786db593b14149292854cc05f
galiapsvwordplay <galiapsvwordplay@gmail.com>
2020-11-15 19:49:20 -0600

    calcular P

--- js/calcular.js
+++ js/calcular.js
@@ -1,12 +1,23 @@
-// @ts-nocheck
+String.prototype.replace=function(index, character) { return this.substr(0, index) + 
+    character + this.substr(index+character.length); } 
+//Uso: palConGuiones = palConGuiones.replaceAt(i*2, letra);
+
 const palabras = ["casa", "perro", "gato", "burro"];
 
 const palabra = palabras[Math.floor(Math.random()*palabras.length)];
 
-const palConGiones=palabra.replace(/./g, "_ ");
+let palConGiones=palabra.replace(/./g, "_ ");
 
-document.querySelector("#calcular").addEventListener('click',() =>
+document.querySelector('#output').innerHTML =palConGiones;
+document.querySelector("#calcular").addEventListener("click" , () =>
 {
-    const letra = document.querySelector("#letra").value;
-    alert(letra)
+    const letra = document.querySelector('#letra').value;
+    
+    for (const i in palabra){
+        if(letra == palabra[i]){
+            palConGiones = palConGiones.replace(i * 2 , letra);
+        }
+    }
+    document.querySelector('#output').innerHTML =palConGiones;
+
 });
