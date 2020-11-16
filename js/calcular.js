15d40592748d0babdfa43fec64d6c2ea84ad3410
galiapsvwordplay <galiapsvwordplay@gmail.com>
2020-11-16 00:02:58 -0600

    calcular

--- js/calcular.js
+++ js/calcular.js
@@ -1,5 +1,8 @@
-String.prototype.replaceAt=function(index, character) { return this.substr(0, index) + 
-    character + this.substr(index+character.length); } 
+String.prototype.replaceAt=function(index, character) 
+{ 
+    return this.substr(0, index) + character + this.substr(index+character.length); 
+} 
+
 //Uso: palConGuiones = palConGuiones.replaceAt(i*2, letra);
 
 const palabras = ["casa", "perro", "gato", "burro"];
@@ -8,14 +11,14 @@ const palabra = palabras[Math.floor(Math.random()*palabras.length)];
 
 let palConGiones=palabra.replace(/./g, "_ ");
 let contFallos = 0;
-document.querySelector('#output').innerHTML =palConGiones;
+document.querySelector('#output').innerHTML = palConGiones;
 document.querySelector('#calcular').addEventListener('click' , () =>
 {
     const letra = document.querySelector('#letra').value;
     let fallo = true;
     for (const i in palabra){   
         if(letra == palabra[i]){
-            palConGiones = palConGiones.replaceAt(i * 2 , letra);
+            palConGiones = palConGiones.replaceAt(i*2, letra);
             fallo=false;
         }
     }
