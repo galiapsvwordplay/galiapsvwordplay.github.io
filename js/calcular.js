6471ce82aae9357cee277a7149f79609273106c6
galiapsvwordplay <galiapsvwordplay@gmail.com>
2020-11-21 19:13:43 -0600

    css

--- js/calcular.js
+++ js/calcular.js
@@ -7,12 +7,3 @@ export class CtrlSaludo {
     return `Hola ${nombreSinEspacios}. `;
   }
 }
-
-function cargarImagen() 
- { 
-
- const imagen = new Image();  
- imagen.src = https://media.giphy.com/media/8vc2rMUDjhy6Y/giphy.gif; 
- return imagen; 
- }
-
